4fc4bad295cecc30467667014787a5b46b22a813
Drejelt <50459306+Drejelt@users.noreply.github.com>
2024-10-14 18:07:03 +0300

    Homework

--- task6.1.py
+++
@@ -1,9 +0,0 @@
-import random
-
-random_list = []
-while len(random_list) < 10:
-    random_list.append(random.randint(-100, 100))
-
-print(random_list)
-
-input ("Нажми Enter дабы завершить выполнение програмы: ")
