cb9c834ab8aa79441170ce0fe459f5ab625523e8
Drejelt <50459306+Drejelt@users.noreply.github.com>
2024-10-14 17:40:09 +0300

    Homework

---
+++ task3.2.py
@@ -0,0 +1,10 @@
+first_name = "Valentyn"
+last_name = "Chernovol"
+
+# Конкатенирую строки
+full_name = first_name + " " + last_name
+
+# Печатаю результат
+print("Hello, " + full_name + "!")
+
+input ('Нажмите Enter для завершения:')
---
+++ task3.3.py
@@ -0,0 +1,27 @@
+a = 10
+b = 3
+
+# Сложение
+addition = a + b
+# Вычитание
+subtraction = a - b
+# Деление
+division = a / b
+# Умноженире
+multiplication = a * b
+# Степень
+exponent = a ** b
+# Остаток от деления
+modulus = a % b
+# Деление с округлением
+floor_division = a // b
+
+# Печатаю резeультат
+print(f"Результат сложения: {a} + {b} = {addition}")
+print(f"Результат вычитания: {a} - {b} = {subtraction}")
+print(f"Результат деления: {a} / {b} = {division}")
+print(f"Результат умножения: {a} * {b} = {multiplication}")
+print(f"Результат степени: {a} ** {b} = {exponent}")
+print(f"Результат остатока от деления (модуль): {a} % {b} = {modulus}")
+print(f"Результат деления с округлением: {a} // {b} = {floor_division}")
+input ('Нажмите Enter для завершения:')
